db47480676518e720f032042ceeec62d34d3ebcf
Onésimo Martins <onesimo.martins@memory.com.br>
2023-10-18 23:20:59 +0100

    chore: first commit

---
+++ src/main/java/com/isptec/psfotos/PsfotosApplication.java
@@ -0,0 +1,13 @@
+package com.isptec.psfotos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsfotosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsfotosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isptec/psfotos/PsfotosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isptec.psfotos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsfotosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
