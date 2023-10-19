d69be01b086d31c77c8c4a193fe363f92ab91277
Onesimo Martins <Ogouveia86@gmail.com>
2023-10-19 12:51:49 +0100

    chore: ajustes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PsFotos-backend
 Backkk
+
+
+para executar as migrações do flyway : ./gradlew flywayMigrate
--- src/main/java/com/isptec/psfotos/web/AlbumController.java
+++ src/main/java/com/isptec/psfotos/web/AlbumController.java
@@ -2,13 +2,17 @@ package com.isptec.psfotos.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RequestMapping("albums")
+@RestController
 public class AlbumController {
 
 
     @GetMapping("fotos")
     public String getFotosUrl(){
+        System.out.println("testeee");
         return "test";
     }
 }
