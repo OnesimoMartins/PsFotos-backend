6bf8b5f62526626418e47036a036a7e096a9c08e
Onesimo Martins <Ogouveia86@gmail.com>
2023-10-19 13:16:24 +0100

    chore: lombok

--- src/main/java/com/isptec/psfotos/domain/domain/entity/Cliente.java
+++ src/main/java/com/isptec/psfotos/domain/domain/entity/Cliente.java
@@ -1,4 +1,14 @@
 package com.isptec.psfotos.domain.domain.entity;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Cliente {
+
+    private Integer id;
+    private String nome;
+
 }
