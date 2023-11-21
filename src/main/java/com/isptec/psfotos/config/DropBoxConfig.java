4a232145d8f8d368a02cb9c5b6ea3087d9e65adc
Onesimo Martinsm <Ogouveia86@gmail.com>
2023-11-21 01:51:49 +0100

    feat: serviço dropBox

--- src/main/java/com/isptec/psfotos/config/DropBoxConfig.java
+++ src/main/java/com/isptec/psfotos/config/DropBoxConfig.java
@@ -1,16 +1,30 @@
 package com.isptec.psfotos.config;
 
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DropBoxConfig {
 
-//    @Value("")
-//    private String appKey;
+    @Value("${psfotos.dbx.appkey}")
+    private String appKey;
+    @Value("${psfotos.dbx.secret}")
+    private String appSecret;
+    @Value("${psfotos.dropbox.accesstoken}")
+    private String dbxAccessToken;
 //
 //    @Value("")
 //    private String appConfig;
 
+
+    @Bean
+    public DbxClientV2 dropBoxClientBean(){
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("psfotos").build();
+        return new DbxClientV2(config, dbxAccessToken);
+    }
 
 
 //    public
--- src/main/java/com/isptec/psfotos/domain/service/DropBoxService.java
+++ src/main/java/com/isptec/psfotos/domain/service/DropBoxService.java
@@ -1,37 +1,128 @@
 package com.isptec.psfotos.domain.service;
 
+import com.dropbox.core.DbxDownloader;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.UploadErrorException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.List;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class DropBoxService {
 
 
-//   private final DbxClientV2 dropboxClient;
-    public String salvarNovoTxt(){
+   private final DbxClientV2 dropboxClient;
 
-        String fileName= UUID.randomUUID().toString();
 
-        List<String> lines = Collections.singletonList("");
+   public String criarNovoCatalogo()  {
+       String fileName= "/catalogo_"+UUID.randomUUID()+".txt";
+       FileMetadata metadata = null;
 
-        Path file = Paths.get(fileName);
+       try (InputStream in = new ByteArrayInputStream("".getBytes())) {
+          metadata = dropboxClient.files()
+                   .uploadBuilder(fileName)
+                   .uploadAndFinish(in);
+       } catch (UploadErrorException e) {
+           System.out.println(" Erro 1");
+       } catch (DbxException | IOException e) {
+           System.out.println("Erro 2");
+       }
+
+       return metadata.getPathDisplay();
+   }
+
+
+    public void atualizarCatalogo(String path, String newString)  {
+        FileMetadata metadata = null;
+
         try {
-            Files.write(file, lines, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+            DbxDownloader downloader= dropboxClient.files()
+                    .download(metadata.getPathDisplay());
 
-//        MultipartFile file = new MultipartFile()
+            String result = new BufferedReader(new InputStreamReader(downloader.getInputStream()))
+                    .lines().parallel().collect(Collectors.joining("\n"));
+
+            StringBuilder stringBuilder = new StringBuilder(result);
+
+            stringBuilder.append("\n");
+            stringBuilder.append(newString);
+
+            dropboxClient.files().deleteV2(metadata.getPathDisplay());
+
+            dropboxClient.files()
+                    .upload(metadata.getPathLower())
+                    .uploadAndFinish(new ByteArrayInputStream(stringBuilder
+                            .toString().getBytes()));
+
+        } catch (UploadErrorException e) {
+            System.out.println(" Erro 1");
+        } catch (DbxException | IOException e) {
+            System.out.println(" Erro 2");
+        }
+    }
+
+    @Deprecated
+    public String salvarNovoTxt2() throws DbxException, IOException {
+
+        String fileName= "/catalogo_"+UUID.randomUUID()+".txt";
+
+        System.out.println(fileName);
+        System.out.println(dropboxClient.users().getCurrentAccount().getEmail());
+
+//        para por byes
+//        InputStream in = new ByteArrayInputStream(fileName.getBytes())
+
+        try (InputStream in = new ByteArrayInputStream("test".getBytes())) {
+
+
+            FileMetadata metadata = dropboxClient.files()
+                    .uploadBuilder(fileName)
+                    .uploadAndFinish(in);
+
+            System.out.println(metadata.getPathLower());
+            System.out.println(metadata.getPathDisplay());
+            System.out.println(metadata.getId());
+
+
+            System.out.println("UU");
+//            FileMetadata metaData2= dropboxClient.files()
+//                    .download(metadata.getPathDisplay())
+//                    .getResult();
+//                    .getF(metadata.getPathDisplay());
+//            metaData2.
+//            metaData2.getExportInfo().getExportAs()
+            DbxDownloader downloader= dropboxClient.files()
+                    .download(metadata.getPathDisplay());
+//            FileOutputStream out = new FileOutputStream(
+//                    new File("C:\\Users\\marti\\OneDrive\\Ambiente de Trabalho\\ISPTEC\\APLM\\local_store\\txt.txt"));
+
+            // Read Data
+            String result = new BufferedReader(new InputStreamReader(downloader.getInputStream()))
+                    .lines().parallel().collect(Collectors.joining("\n"));
+
+            result= result+" 0000000000000000000000";
+
+            dropboxClient.files().deleteV2(metadata.getPathDisplay());
+
+
+            dropboxClient.files()
+             .upload(metadata.getPathLower())
+               .uploadAndFinish(new ByteArrayInputStream(result.getBytes()));
+
+            System.out.println(result);
+//            inputStream.
+
+        }
 
         return  fileName;
 
