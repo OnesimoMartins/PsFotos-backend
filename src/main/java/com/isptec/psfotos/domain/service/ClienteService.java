ba258b62a232ac55f45c473eaacc3979398c25c0
Onesimo Martinsm <Ogouveia86@gmail.com>
2023-11-22 00:56:57 +0100

    fix: conectar amigos

--- src/main/java/com/isptec/psfotos/domain/service/ClienteService.java
+++ src/main/java/com/isptec/psfotos/domain/service/ClienteService.java
@@ -4,6 +4,7 @@ import com.isptec.psfotos.domain.entity.Cliente;
 import com.isptec.psfotos.domain.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.util.Objects;
@@ -26,7 +27,8 @@ public class ClienteService {
                 .orElse(null);
     }
 
-    public final Boolean conectarAmigos(Integer id1, Integer id2){
+    @Transactional
+    public Boolean conectarAmigos(Integer id1, Integer id2){
        Optional<Cliente> c = clienteRepository.findAmigos(id1)
                .stream().filter(it->
                        Objects.equals(it.getId(),id2)
@@ -38,7 +40,9 @@ public class ClienteService {
                "insert into cliente_amigos(cliente,cliente2) values (%s,%s);",
                id1,id2
        );
-       int rows = entityManager.createQuery(sql).executeUpdate();
+
+        System.out.println(sql);
+       int rows = entityManager.createNativeQuery(sql).executeUpdate();
         return rows > 0;
     }
 
--- src/main/java/com/isptec/psfotos/web/AlbumController.java
+++ src/main/java/com/isptec/psfotos/web/AlbumController.java
@@ -5,11 +5,14 @@ import com.isptec.psfotos.domain.service.AlbumService;
 import com.isptec.psfotos.domain.entity.Album;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.isptec.psfotos.web.dto.AlbumDto;
+import org.springframework.web.multipart.MultipartFile;
 
 
 @RequestMapping("albums")
@@ -23,6 +26,12 @@ public class AlbumController {
     public String getFotosUrl(){
         System.out.println("testeee");
         return "test";
+    }
+
+    @PutMapping("{id}/imagem")
+    public Boolean uploadImagem(@PathVariable Integer id, MultipartFile image){
+        System.out.println("testeee");
+        return true;
     }
 
     @PostMapping
